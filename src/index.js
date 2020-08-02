ecd49c1b6f880dc1c6ac15d701f182bc47bb1054
mokyoungg <mokyoungg@gmail.com>
2020-08-02 17:00:13 +0900

    [ADD] modernReact section 4 - 5
    - 함수형 컴포넌트, class형 컴포넌트로 리팩토링
    - 사용자의 지리적 위치 알아내는 코드 작성

--- src/index.js
+++ src/index.js
@@ -1,47 +1,49 @@
-// Import the React and ReactDom libraries
 import React from "react";
 import ReactDom from "react-dom";
-import faker from "faker";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
 
-// Creat a react component
+/*
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <div>
-          <h4>Warning!</h4>
-          Are you sure you want to do this?
-        </div>
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Sam"
-          timeAgo="Today at 4:45PM"
-          content="Nice blog Post"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Alex"
-          timeAgo="Today at 2:00PM"
-          content="Good"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Jane"
-          timeAgo="Yesterday"
-          content="Bravo"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-    </div>
+  //현재 위치 찾는 함수
+  window.navigator.geolocation.getCurrentPosition(
+    (postion) => console.log(postion),
+    (err) => console.log(err)
   );
-};
 
-// Take the react component and show it on the screen
+  return <div>위도: </div>;
+};
+*/
+
+//클래스형 컴포넌트로 리팩토링
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // This is the only time we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: "" }; //lat = 위도
+
+    window.navigator.geolocation.getCurrentPosition(
+      (postion) => {
+        // we called setState!!
+        this.setState({ lat: postion.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  //React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>위도: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
+
 ReactDom.render(<App />, document.querySelector("#root"));
