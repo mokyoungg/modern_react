20acc5bc5924a52d3caef29bbce9824f3bdb2b80
mokyoungg <mokyoungg@gmail.com>
2020-08-04 21:30:52 +0900

    [ADD] section8.Making API Requests with React
    - Axios 설치 및 활용
    - Axios를 활용한 데이터 로드
    - unsplash.js를 활용하여 axios코드 리팩터링

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
 import React from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  //비동기 대기 구문이 더 쓰기 쉽다.(2번)
+  // 기존 코드 async onSearchSubmit(term)
+
+  // 화살표 함수와 async 활용
+  onSearchSubmit = async (term) => {
+    //axios 코드 리팩터링
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+      /*기존 코드 (api 폴더, unsplash.js 참고)
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term }, 
+      headers: {
+        Authorization: "Client-ID eZ5vvtbknlFwjw0WT8QCwTHse9f_d0ppyX5Cc2zTarA",
+      },*/
+    });
+    /*비동기 요청을 받는 방법(1번)
+      .then((response) => {
+        console.log(response.data.results);
+      });*/
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
