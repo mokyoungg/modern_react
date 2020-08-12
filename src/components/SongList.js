733711e578d3ebbf3e1bdec9b61fd34cb8d96fd2
mokyoungg <mokyoungg@gmail.com>
2020-08-12 15:47:22 +0900

    [ADD] SongDetail 컴포넌트 추가
    - SongDetail 컴포넌트 추가
    - Redux를 활용하여 데이터 받아오기

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -18,16 +24,19 @@ class SongList extends Component {
   render() {
     // this.props === {songs: state.songs}
     //console.log(this.props.songs);
-
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong: selectSong,
+})(SongList);
+
 /* function connect(){
     return function(){
         return 'Hi there'
