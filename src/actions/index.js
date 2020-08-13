3b89caf8bd4da68e5bbd59ff15473b6e927b006a
mokyoungg <mokyoungg@gmail.com>
2020-08-13 19:25:23 +0900

    [ADD] section 18. Async Actions with Redux Thunk
    - Redux Thunk 설치
    - Redux Thunk를 활용한 비동기 Action creator 코드

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,21 @@
-// Action creator
-export const selectSong = (song) => {
-  //Return an action
-  return {
-    type: "SONG_SELECTED",
-    payload: song,
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+/*최초 코드
+export const fetchPosts = () => {
+  // Bad approach!
+  // Error: Actions must be plain objects. Use custom middleware for async actions.
+  const promise = await jsonPlaceholder.get("/posts"); 
+
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get("/posts");
+
+    dispatch({ type: "FETCH_POSTS", payload: response });
   };
+};*/
+
+// 코드 리팩토링
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
