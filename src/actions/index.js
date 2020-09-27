579b04cf5775ca1371f63174040f95f5aa3b7763
mokyoungg <mokyoungg@gmail.com>
2020-09-27 22:47:42 +0900

    [ADD] section18 복습
    - Redux Thunk 설치
    - 비동기와 Redux 함께 사용하기.(actions/index.js 참조)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,25 @@
-// Action creator
-export const selectSong = (song) => {
-  // Return an action
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+/* Code
+export const fetchPosts = async () => {
+  // Bad approach! >> Actions must be plain object!
+  const response = await jsonPlaceholder.get("/posts");
   return {
-    type: "SONG_SELECTED",
-    payload: song,
+    type: "FETCH_POSTS",
+    payload: promise,
   };
+
+  // 비동기 구문(redux thunk)
+  return async function (dispatch, getState) {
+    const response = await jsonPlaceholder.get("/posts");
+
+    dispatch({ type: "FETCH_POSTS", payload: response });
+  };
+};*/
+
+// 리팩토링
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
