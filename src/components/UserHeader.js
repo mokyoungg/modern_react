4691686ce05e8ea7d32464cd4d75dd8a0543933b
mokyoungg <mokyoungg@gmail.com>
2020-09-28 17:12:03 +0900

    [ADD] Section19. Redux Store Design

---
+++ src/components/UserHeader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { connect } from "react-redux";
+//import { fetchUser } from "../actions";
+
+class UserHeader extends React.Component {
+  /* 다음의 코드는 서버 요청이 너무 많다.(중복요청이 많다.),
+  lodash 라이브러리 사용
+  componentDidMount() {
+    //this.props.fetchUser(this.props.userId);
+  }*/
+
+  render() {
+    //const user = this.props.users.find((user) => user.id === this.props.userId);
+    const { user } = this.props;
+
+    if (!user) {
+      return null;
+    }
+
+    return <div className="header">{user.name}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
+};
+
+/*
+const mapStateToProps = (state) => {
+  return { users: state.users };
+};*/
+
+export default connect(mapStateToProps)(UserHeader);
