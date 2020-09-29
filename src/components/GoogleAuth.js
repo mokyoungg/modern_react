d36a7ec632565135e2a7cd7fe3f866448a48676a
mokyoungg <mokyoungg@gmail.com>
2020-09-29 15:54:21 +0900

    [TEST] Google KEY
    - 구글 키를 위한  gitignore 테스트

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    return test;
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
